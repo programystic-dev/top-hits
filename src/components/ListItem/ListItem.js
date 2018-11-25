4292364336d49cac7acaca8d9cc7eeddf81f923f
graffme <daria.rucka@gmail.com>
2018-11-25 22:44:42 +0100

    Add responsive styles

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -10,10 +10,12 @@ const ListItem = ({ item, index }) => (
     <LazyLoad height={100} throttle={100}>
       <div className={classNames(styles.fade, styles.listItem)}>
         <div className={styles.itemNumber}>{index + 1}</div>
-        <Avatar alt="Album label" size="small" src={item['im:image'][1].label} />
-        <div className={styles.itemDesc}>
-          <h2 className={styles.descTitle}>{item.title.label}</h2>
-          <p className={styles.descArtist}>{item['im:artist'].label}</p>
+        <div className={styles.infoWrapper}>
+          <Avatar alt="Album label" size="small" src={item['im:image'][1].label} />
+          <div className={styles.itemDesc}>
+            <h2 className={styles.descTitle}>{item.title.label}</h2>
+            <p className={styles.descArtist}>{item['im:artist'].label}</p>
+          </div>
         </div>
       </div>
     </LazyLoad>
