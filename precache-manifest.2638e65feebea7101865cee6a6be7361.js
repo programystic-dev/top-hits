ce005e20ea84af42f7ef5b5f02f29d19255e7463
graffme <daria.rucka@gmail.com>
2018-11-26 12:27:49 +0100

    Updates

---
+++ precache-manifest.2638e65feebea7101865cee6a6be7361.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "62513ccad872467c0d2b4538b92cf44b",
+    "url": "/top-hits/static/media/background.62513cca.png"
+  },
+  {
+    "revision": "eb17ebd33486c03420ba",
+    "url": "/top-hits/static/js/runtime~main.eb17ebd3.js"
+  },
+  {
+    "revision": "a251524c4d1c1eea660f",
+    "url": "/top-hits/static/js/main.a251524c.chunk.js"
+  },
+  {
+    "revision": "e0a6a8d77e38645c9da1",
+    "url": "/top-hits/static/js/1.e0a6a8d7.chunk.js"
+  },
+  {
+    "revision": "a251524c4d1c1eea660f",
+    "url": "/top-hits/static/css/main.504b0d98.chunk.css"
+  },
+  {
+    "revision": "e0a6a8d77e38645c9da1",
+    "url": "/top-hits/static/css/1.108bef68.chunk.css"
+  },
+  {
+    "revision": "63eef8c96d4316084adee0d60bccf1a0",
+    "url": "/top-hits/index.html"
+  }
+];
