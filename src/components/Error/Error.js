d5eab5efc576cae0189f280e27bb60f243918656
graffme <daria.rucka@gmail.com>
2018-11-29 11:38:13 +0100

    Add Error component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Error.module.scss';
+
+const Error = () => (
+  <div className={styles.error}>I'm sorry! Error occured :(</div>
+)
+
+export default Error;
